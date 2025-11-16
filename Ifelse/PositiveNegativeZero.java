97a05eed07b52e65394e5ebf8a27631245d3bb0d
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-16 14:02:23 +0530

    if else started

---
+++ Ifelse/PositiveNegativeZero.java
@@ -0,0 +1,15 @@
+package Ifelse;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int n = 6;
+
+        if (n > 0) {
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        }else {
+            System.out.println("Zero");
+        }
+    }
+}
