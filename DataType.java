ffdbc26bdb3b3dc5a6c4a237e9b4085da1b6135e
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-16 12:15:27 +0530

    Airthmetic operator

---
+++ DataType.java
@@ -0,0 +1,21 @@
+
+public class DataType {
+    public static void main(String[] args) {
+        // name - 'saurabh bansal ' // word/sentence
+        // age - 24 // number
+        // gender - m // character
+        // percetage - 93.5 // decimal
+        // isWell - true // boolean
+
+        // DataType variableName = data ;
+
+        String name = "Saurabh Bansal";
+        int age = 24;
+        char gender = 'm';
+        double percetage = 93.5;
+        boolean isWell = true; // true/false
+
+        System.out.println(name);
+
+    }
+}
---
+++ Operator.java
@@ -0,0 +1,16 @@
+public class Operator {
+    public static void main(String[] args) {
+        // Arithemtic Operator , + , - , * , / , %
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.println(mod);
+    }
+}
