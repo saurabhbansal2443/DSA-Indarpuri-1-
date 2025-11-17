c6c0ed0dc2e9a2f1a8c70109ea5c6d056e4eb0cc
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-17 19:07:55 +0530

    Question 1 Ifelse

--- Ifelse/PositiveNegativeZero.java
+++ Ifelse/PositiveNegativeZero.java
@@ -1,14 +1,18 @@
 package Ifelse;
 
+import java.util.*;
+
 public class PositiveNegativeZero {
     public static void main(String[] args) {
-        int n = 6;
+        // Write a program to check whether a number is negative, positive or zero
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
         if (n > 0) {
             System.out.println("Positive");
         } else if (n < 0) {
             System.out.println("Negative");
-        }else {
+        } else {
             System.out.println("Zero");
         }
     }
