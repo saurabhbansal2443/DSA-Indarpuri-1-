a451c0dd2b101fe4bbd69dda7d042836a211a697
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-17 17:52:02 +0530

    Backup Complete

---
+++ OperatorsBackup.java
@@ -0,0 +1,46 @@
+public class OperatorsBackup {
+    public static void main(String[] args) {
+        // Arithemtic Operator + , - , * , / , %
+        // int a = 10;
+        // int b = 20;
+
+        // int sum = a + b;
+        // int sub = a - b;
+        // int mult = a * b;
+        // int div = a / b;
+        // int rem = a % b;
+
+        // System.out.println(rem);
+
+        // Comparision Operator < , > , <= , >= , == , !=
+
+        // int a = 20;
+        // int b = 20;
+
+        // System.out.println(a != b);
+
+        // Assignment Operator , = , += , -= , *= , /= , %=
+
+        // int a = 10;
+
+        // a = a + 20;
+
+        // a %= 20;
+
+        // System.out.println(a);
+        // Increment/Decrement Operator
+
+        int a = 5;
+
+        System.out.println(--a);
+        System.out.println(--a);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(++a);
+
+    }
+}
